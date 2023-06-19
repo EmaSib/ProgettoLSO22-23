52b214e43559ccb99b0d424886d9964285e6b348
EmaSib <ema.sibilio@studenti.unina.it>
2023-06-19 17:40:37 +0200

    Fix al build:gradle -> compileSdk e targetSdk al valore 33. Aggiornate le dependencies:  implementation 'androidx.appcompat:appcompat:1.6.0' -> 1.6.1  implementation 'com.google.android.material:material:1.7.0' -> 1.9.0 Commentate delle righe nel manifest per dei warning: nel caso di necessita si possono decommentare. Aggiornata la versione di Android Gradle Plugin Version dal 7.2.1 al 7.2.2

--- app/src/main/java/com/code/progettolso22_23/gui/Login.java
+++ app/src/main/java/com/code/progettolso22_23/gui/Login.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.progettolso22_23.R;
+import com.code.progettolso22_23.R;
 
 public class Login extends AppCompatActivity {
 
