013206ca971b8cc0d23efec47eb69b2dcf67bf1f
korban98 <giusepp.passaro@studenti.unina.it>
2023-06-20 13:48:24 +0200

    Commit layouts for "Login" and "Registrazione"

---
+++ app/src/main/java/com/code/progettolso22_23/gui/Registrazione.java
@@ -0,0 +1,18 @@
+package com.code.progettolso22_23.gui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.code.progettolso22_23.R;
+
+public class Registrazione extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrazione);
+    }
+
+}
